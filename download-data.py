e613fc7b11251e6af9aae9b2faca325c768d9d20
abdaniel <abrahamdaniel92@gmail.com>
2017-03-24 22:33:18 -0500

    Removed unused import in download-data.py

--- download-data.py
+++ download-data.py
@@ -1,12 +1,13 @@
 from subprocess import call
 
-import wget
 from itertools import groupby
 from operator import itemgetter
 
 global_path = '/Users/daniel/IdeaProjects/cs766-project-data/'
 
-with open('/Users/daniel/IdeaProjects/cs766-project-data/vis16cat.txt') as f:
+file_name = '/Users/daniel/IdeaProjects/cs766-project-data/vis16cat.txt'
+
+with open('%s' % file_name) as f:
     lines = f.read().splitlines()
 
 lines = [x.split('\t') for x in lines]
@@ -14,6 +15,5 @@ lines = [x.split('\t') for x in lines]
 for elt, items in groupby(lines, itemgetter(0)):
     output_directory = global_path + elt
     for i in items:
-        url = "wget {0} -P {1}".format(i[1], output_directory)
+        url = "wget -nc {0} -P {1} --tries=1".format(i[1], output_directory)
         call(url, shell=True)
-
